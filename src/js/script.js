44330b72bb5d1f12c43e32c65ec97a3b45474fe9
Fabio <fabioprogerio@gmail.com>
2025-04-01 09:50:03 -0300

    Adicionando operadores aritimeticos e logicos

--- src/js/script.js
+++ src/js/script.js
@@ -43,7 +43,8 @@ let numString ="547.987";
 console.log(parseFloat(numString));
 
 //float => string
-let nmFloat1 =554.665;
+
+let numFloat1 =554.665;
 console.log(numFloat1.toString());
 
 let numInt=100;
@@ -52,3 +53,36 @@ console.log(numInt.toString())
 //Metodos - Parte 1
 
 //Length - Verifica o tamanho da string
+
+let frase ="O mundo da tecnologia";
+console.log(frase.length)
+
+//indexOf - retorna um trecho de um texto
+
+let texto ="Programação sustenstavel"
+console.log(texto.indexOf("a"))
+
+//slice retorna parte de um texto apontando o inicio e o final
+
+let info="processamento de ponta"
+console.log(info.slice(0,13))
+
+//OPERADORES ARITIMETICO
+
+const num1 =10;
+const num2 =20;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+//OPERADORES LÓGICOS
+
+const num3 =20;
+const num4 =30;
+
+console.log(num3 > num4);
+console.log(num3 < num4 && num4 < 50);
+console.log(num3 > num4 || num4 > num3);
+console.log(num3 == num4 || num4 <= num3)
