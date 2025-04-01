b0eb79f4161c33389a2075a47dd4265fe5c411a3
Fabio <fabioprogerio@gmail.com>
2025-04-01 09:28:36 -0300

    adicionando conversões

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,37 @@ console.log(valor)
 let exemplo1 ={};
 
 let exemplo2 ={};
+
+//TIPOS VARIAVEIS
+let exemplo3 =10;
+console.log(typeof exemplo3)
+
+let exemplo4="Aula"
+console.log(typeof exemplo4)
+
+let exemplo5 =true;
+console.log(typeof exemplo5)
+
+let exemplo6 =["huguinho","zezinho","luizinho"];
+console.log(typeof exemplo6)
+
+//CONVERSÕES
+
+//float => inteiro
+let numFloat =123.456;
+console.log(parseInt(numFloat));
+
+//string => float
+let numString ="547.987";
+console.log(parseFloat(numString));
+
+//float => string
+let nmFloat1 =554.665;
+console.log(numFloat1.toString());
+
+let numInt=100;
+console.log(numInt.toString())
+
+//Metodos - Parte 1
+
+//Length - Verifica o tamanho da string
