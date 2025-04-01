e59bd7d7f324dad7ba2b409deef0fb06468a6b63
Fabio <fabioprogerio@gmail.com>
2025-04-01 09:09:58 -0300

    Declaração e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,20 @@
-// alert("Hello World")
-
 //DECLARAÇÃO E VARIAVEIS
 
-var nome="Fiap";
+var nome ="Fiap";
 console.log(nome)
+
+let idade=22;
+console.log(idade)
+
+const sobrenome ="cidade";
+console.log(sobrenome)
+
+let aula;
+console.log(aula)
+
+let valor =null;
+console.log(valor)
+
+let exemplo1 ={};
+
+let exemplo2 ={};
