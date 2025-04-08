b18fb795a53a2851cc6f6149ca2b29d60a22a0a7
Fabio <fabioprogerio@gmail.com>
2025-04-08 08:45:34 -0300

    Adicionando operadores de comparação

--- src/js/script.js
+++ src/js/script.js
@@ -86,3 +86,12 @@ console.log(num3 > num4);
 console.log(num3 < num4 && num4 < 50);
 console.log(num3 > num4 || num4 > num3);
 console.log(num3 == num4 || num4 <= num3)
+
+// OPERADORES DE COMPARAÇÃO
+
+const num5 =25
+const num6 =35
+
+console.log(num5 == num6) //compara
+console.log(num5 === num6) //compara e verifica o tipo de variavel
+console.log(num5!= num6) //diferente
